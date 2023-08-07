8de2e4cb5478c4df29f9a99fe5d4011798b9cfc0
r37 <vaibhavbhalala1212@gmail.com>
2023-08-07 17:03:40 +0530

    Third Commit

--- lib/Quotes_screen.dart
+++ lib/Quotes_screen.dart
@@ -24,54 +24,85 @@ class _QuotesScreenState extends State<QuotesScreen> {
   @override
   Widget build(BuildContext context) {
     int count = 1;
-    int i = 0;
+    bool istoggled = false;
+
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                actions: [
-                  OutlinedButton(
-                      onPressed: () {
-                        Navigator.pushNamedAndRemoveUntil(
-                            context, '/', (route) => false);
-                      },
-                      child: Text("Yes")),
-                  OutlinedButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text("No")),
-                ],
-                content: Text(
-                  "Are you sure to Exit???",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-              ),
-            );
-          },
-          icon: Icon(Icons.arrow_back),
-        ),
-        title: Text("Quotes App"),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: Quotedata.map((e) => Card(
-                margin: EdgeInsets.all(10),
-                child: Column(
-                  children: [
-                    Text("${count++} : ${e.quote}"),
-                    Text("${e.author}"),
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  actions: [
+                    OutlinedButton(
+                        onPressed: () {
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, '/', (route) => false);
+                        },
+                        child: Text("Yes")),
+                    OutlinedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("No")),
                   ],
+                  content: Text(
+                    "Are you sure to Exit???",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
                 ),
-                elevation: 4,
-              )).toList(),
+              );
+            },
+            icon: Icon(Icons.arrow_back),
+          ),
+          title: Text("Quotes App"),
+          centerTitle: true,
         ),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.white.withOpacity(0.2),
+            onPressed: () {
+              setState(() {
+                istoggled = !istoggled;
+              });
+            },
+            child: (istoggled) ? Icons.grid_3x3 : Icons.list),
+        body: Scrollbar(
+          radius: Radius.circular(10),
+          interactive: true,
+          thickness: 10,
+          child: ListView.builder(
+              itemCount: Quotedata.length,
+              itemBuilder: (BuildContext context, int i) => Card(
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: Colors.pinkAccent,
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                  "https://w0.peakpx.com/wallpaper/573/4/HD-wallpaper-black-ornaments-texture-black-background-floral-ornaments-background-black-texture.jpg"),
+                              fit: BoxFit.cover)),
+                      alignment: Alignment.center,
+                      height: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Text(
+                            "${Quotedata[i].quote}",
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white.withOpacity(0.50)),
+                          ),
+                          Text(
+                            "- ${Quotedata[i].author}",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white.withOpacity(0.50)),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )),
+        ));
   }
 }
--- lib/components/utils/globals.dart
+++ lib/components/utils/globals.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../../model/quotes_model.dart';
+
 class Globals {
   static Color themecolor = Colors.orange;
+
+  late List<Quotes> Quotedata;
 }
